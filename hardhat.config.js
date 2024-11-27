29c874a6b2b12a9cc11e9edd16a2a5056efc1bbe
Akash Jag <akashjagannathan408@gmail.com>
2024-11-27 02:39:00 -0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    shapeSepolia: {
+      url: process.env.SHAPE_SEPOLIA_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  //   etherscan: {
+  //     apiKey: process.env.ETHERSCAN_API_KEY,
+  //   },
+};
